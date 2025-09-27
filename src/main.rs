8b00864617e842df3a1f215290097bfb020c880a
gcwyrostek <gcwyrostek@gmail.com>
2025-09-27 02:21:11 -0400

    Added tiling.rs and associated ground texture.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{prelude::*, window::PresentMode};
 
 // Game modules
 mod player;
+mod tiling;
 //mod enemy;
 //mod bullet;
 //mod reticle;
@@ -10,12 +11,15 @@ mod player;
 //mod guns;
 //mod revive_kit_pickup;
 
+const WIN_W: f32 = 1280.;
+const WIN_H: f32 = 720.;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Untitled Survival Shooter".into(),
-                resolution: (1280., 720.).into(),
+                resolution: (WIN_W, WIN_H).into(),
                 present_mode: PresentMode::AutoVsync,
                 ..default()
             }),
@@ -23,6 +27,7 @@ fn main() {
         }))
         // Core game systems
         .add_systems(Startup, player::setup_player)
+        .add_systems(Startup, tiling::setup)
         .add_systems(Update, player::player_movement)
         .run();
 } 
