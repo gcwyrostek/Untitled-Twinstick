fc8a19bb66c00397e4f5c9a877548e2d69eef252
ifemi13 <ifemiok@outlook.com>
2025-10-06 01:45:33 -0400

    updated version

---
+++ src/menu.rs
@@ -0,0 +1,246 @@
+use crate::GameState;
+use bevy::prelude::*;
+
+use bevy::input::ButtonInput;
+use bevy::input::keyboard::KeyCode;
+
+#[derive(Component)]
+enum MenuButton {
+    Host,
+    Join,
+    Credits,
+    Exit,
+}
+
+// tags UI elements for cleanup
+#[derive(Component)]
+struct MenuUI;
+
+pub struct MenuPlugin;
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            // blank menu enter/exit logs
+            .add_systems(OnEnter(GameState::Menu), on_enter_menu)
+            .add_systems(OnExit(GameState::Menu), on_exit_menu)
+            // Menu: start --> click / Enter / Space
+            .add_systems(Update, start_on_input.run_if(in_state(GameState::Menu)))
+            // Menu: quit --> Esc / Q / Backspace
+            .add_systems(
+                Update,
+                quit_to_menu_on_input.run_if(in_state(GameState::Playing)),
+            )
+            // optional logs on playing
+            .add_systems(OnEnter(GameState::Playing), on_enter_playing)
+            .add_systems(OnExit(GameState::Playing), on_exit_playing);
+    }
+}
+
+fn on_enter_menu(mut commands: Commands) {
+    info!("STATE: MENU (blank). Click or press Enter/Space to START.");
+    commands.spawn((Camera2d, MenuUI));
+
+    // creates a UI containter that fills the 100% of the window (width and height)
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            BackgroundColor(Color::srgb(0.1, 0.1, 0.1)),
+            MenuUI,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Text::new("Untitled Twinstick Shooter"),
+                TextFont {
+                    font_size: 64.0,
+                    ..default()
+                },
+                TextColor(Color::WHITE),
+                Node {
+                    margin: UiRect::bottom(Val::Px(50.0)),
+                    ..default()
+                },
+            ));
+
+            // container for buttons
+            parent
+                .spawn(Node {
+                    flex_direction: FlexDirection::Column,
+                    align_items: AlignItems::Center,
+                    row_gap: Val::Px(20.0),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // host button
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Px(200.0),
+                                height: Val::Px(50.0),
+                                border: UiRect::all(Val::Px(2.0)),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                ..default()
+                            },
+                            BorderColor(Color::WHITE),
+                            BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+                            MenuButton::Host,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("HOST"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                TextColor(Color::WHITE),
+                            ));
+                        });
+
+                    // join button
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Px(200.0),
+                                height: Val::Px(50.0),
+                                border: UiRect::all(Val::Px(2.0)),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                ..default()
+                            },
+                            BorderColor(Color::WHITE),
+                            BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+                            MenuButton::Join,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("JOIN"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                TextColor(Color::WHITE),
+                            ));
+                        });
+
+                    // credits button
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Px(200.0),
+                                height: Val::Px(50.0),
+                                border: UiRect::all(Val::Px(2.0)),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                ..default()
+                            },
+                            BorderColor(Color::WHITE),
+                            BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+                            MenuButton::Credits,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("CREDITS"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                TextColor(Color::WHITE),
+                            ));
+                        });
+
+                    // exit button
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Px(200.0),
+                                height: Val::Px(50.0),
+                                border: UiRect::all(Val::Px(2.0)),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                ..default()
+                            },
+                            BorderColor(Color::WHITE),
+                            BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+                            MenuButton::Exit,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("EXIT"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                TextColor(Color::WHITE),
+                            ));
+                        });
+                });
+        });
+}
+
+fn on_exit_menu(mut commands: Commands, query: Query<Entity, With<MenuUI>>) {
+    info!("Leaving MENU...");
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
+    }
+}
+
+fn on_enter_playing() {
+    info!("STATE: PLAYING. Press Esc/Q/Backspace to QUIT to MENU.");
+}
+fn on_exit_playing() {
+    info!("Leaving PLAYING → back to MENU.");
+}
+
+fn start_on_input(
+    mut interaction_query: Query<
+        (&Interaction, &MenuButton),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut next_state: ResMut<NextState<GameState>>,
+    mut exit: EventWriter<bevy::app::AppExit>,
+) {
+    for (interaction, menu_button) in &mut interaction_query {
+        if *interaction == Interaction::Pressed {
+            match menu_button {
+                MenuButton::Host => {
+                    info!("host button pressed.");
+                    next_state.set(GameState::Playing);
+                }
+                MenuButton::Join => {
+                    info!("join button pressed.");
+                }
+                MenuButton::Credits => {
+                    info!("credits button pressed.");
+                    next_state.set(GameState::Credits);
+                }
+                MenuButton::Exit => {
+                    info!("exit button pressed.");
+                    exit.write(bevy::app::AppExit::Success);
+                }
+            }
+        }
+    }
+}
+
+fn quit_to_menu_on_input(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    if keys.just_pressed(KeyCode::Escape)
+        || keys.just_pressed(KeyCode::KeyQ)
+        || keys.just_pressed(KeyCode::Backspace)
+    {
+        next_state.set(GameState::Menu);
+    }
+}
--- src/slideshow.rs
+++ src/slideshow.rs
@@ -1,6 +1,23 @@
+use crate::GameState;
 use std::time::Duration;
 
-use bevy::{prelude::*, window::PresentMode};
+use bevy::prelude::*;
+
+pub struct CreditsPlugin;
+
+impl Plugin for CreditsPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(MyTimer(Timer::new(
+            Duration::from_secs(2),
+            TimerMode::Repeating,
+        )))
+        .init_resource::<MyImageArray>() // Initializes this array with all placeholder entities! See above for more info...
+        .init_resource::<MyCounter>() // Does the same with the counter, initializing to 0.
+        .add_systems(OnEnter(GameState::Credits), setup)
+        .add_systems(OnExit(GameState::Credits), cleanup_credits)
+        .add_systems(Update, countdown.run_if(in_state(GameState::Credits)));
+    }
+}
 
 #[derive(Resource)]
 struct MyTimer(Timer);
@@ -16,6 +33,10 @@ struct MyCounter {
     value: usize,
 }
 
+// tags the camera and images for cleanup later
+#[derive(Component)]
+struct CreditsUI;
+
 // This is a trait for MyImageArray. It's needed for .init_resource in App::new(), so rust knows what to initialize the array to.
 // see more about "Entity::PLACEHOLDER" here: (https://docs.rs/bevy/latest/bevy/ecs/entity/struct.Entity.html)
 // (also just a great place for more bevy info)
@@ -27,105 +48,116 @@ impl Default for MyImageArray {
     }
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Team Blueberry Credits Slideshow".into(),
-                resolution: (1280., 720.).into(),
-                present_mode: PresentMode::AutoVsync,
-                ..default()
-            }),
-            ..default()
-        }))
-        .insert_resource(MyTimer(Timer::new(
-            Duration::from_secs(2),
-            TimerMode::Repeating,
-        )))
-        .init_resource::<MyImageArray>() // Initializes this array with all placeholder entities! See above for more info...
-        .init_resource::<MyCounter>() // Does the same with the counter, initializing to 0.
-        .add_systems(Startup, setup)
-        .add_systems(Update, countdown)
-        .run();
-}
-
 // The arguments of this system (that's what Bevy calls functions) takes both an immutable and a mutable resource.
 // This is how systems get global information. Global variables must be packaged within resource.
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut image_array_resource: ResMut<MyImageArray>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut image_array_resource: ResMut<MyImageArray>,
+    mut counter: ResMut<MyCounter>,
+) {
+    counter.value = 0;
+
     // Spawn the camera...so we can see stuff
-    commands.spawn(Camera2d);
+    commands.spawn((Camera2d, CreditsUI));
     // Time to populate our array!
     // Here we spawn the image entities and assign them to the array. Notice how we're using .id() to get reference to entity.
     // I spawn them underground (at y = -1000.) initially, and bring them to (0., 0., 0.) in countdown()!!
-    image_array_resource.image_array[0] = commands.spawn((
-        Sprite::from_image(asset_server.load("amyia.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[1] = commands.spawn((
-        Sprite::from_image(asset_server.load("daniel.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[2] = commands.spawn((
-        Sprite::from_image(asset_server.load("gordon.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[3] = commands.spawn((
-        Sprite::from_image(asset_server.load("matthew.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[4] = commands.spawn((
-        Sprite::from_image(asset_server.load("ifemi.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[5] = commands.spawn((
-        Sprite::from_image(asset_server.load("peter.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[6] = commands.spawn((
-        Sprite::from_image(asset_server.load("vlad.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
-    image_array_resource.image_array[7] = commands.spawn((
-        Sprite::from_image(asset_server.load("secret.png")),
-        Transform {
-            translation: Vec3::new(0., -1000., 0.),
-            ..default()
-        },
-    )).id();
+    image_array_resource.image_array[0] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/amyia.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[1] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/daniel.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[2] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/gordon.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[3] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/matthew.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[4] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/ifemi.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[5] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/peter.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[6] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/vlad.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
+    image_array_resource.image_array[7] = commands
+        .spawn((
+            Sprite::from_image(asset_server.load("slideshow/secret.png")),
+            Transform {
+                translation: Vec3::new(0., -1000., 0.),
+                ..default()
+            },
+            CreditsUI,
+        ))
+        .id();
 }
 
 // This system needs a query to a mutable transform (because we want to change the image's position to 0, 0, 0! it spawns underground initially.)
 fn countdown(
-            mut commands: Commands,
-            mut timer: ResMut<MyTimer>,
-            time: Res<Time>, 
-            mut counter: ResMut<MyCounter>, 
-            image_array_resource: ResMut<MyImageArray>, 
-            mut query: Query<&mut Transform>) 
-{
-    // Let's stop when we've got through all 8 images.
+    mut commands: Commands,
+    mut timer: ResMut<MyTimer>,
+    time: Res<Time>,
+    mut counter: ResMut<MyCounter>,
+    image_array_resource: ResMut<MyImageArray>,
+    mut query: Query<&mut Transform>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    // Let's stop when we've got through all 8 images. Return back to the menu.
     if counter.value >= 8 {
+        next_state.set(GameState::Menu);
         return;
     }
 
@@ -146,5 +178,15 @@ fn countdown(
         commands.entity(current_image).despawn();
         println!("Finished showing image {}", counter.value);
         counter.value += 1;
+    }
+}
+
+fn cleanup_credits(
+    mut commands: Commands, 
+    query: Query<Entity, With<CreditsUI>>
+) {
+    info!("Cleaning up credits slideshow");
+    for entity in &query {
+        commands.entity(entity).despawn();
     }
 }
