f6b196243c6fc2dc96176276cf752e2d805aab8c
gcwyrostek <gcwyrostek@gmail.com>
2025-11-27 11:33:13 -0500

    Added Bullet Collision with Walls

--- src/collisions.rs
+++ src/collisions.rs
@@ -1,4 +1,4 @@
-use crate::{GameState, components::KinematicCollider, components::StaticCollider, player::Player,};
+use crate::{GameState, components::KinematicCollider, components::StaticCollider, player::Player, projectile::Projectile};
 use bevy::{math::bounding::Aabb2d, math::bounding::IntersectsVolume, prelude::*};
 
 pub struct CollisionsPlugin;
@@ -39,8 +39,11 @@ pub fn find_mtv(pushee: &Aabb2d, pusher: &Aabb2d) -> Vec2 {
     }
 }
 
+//Player collisions have been removed from here and placed in player movement, in order to be able to resimulate from inputs for rollback
 pub fn do_collisions(
-    kinematics: Query<(&KinematicCollider, &mut Transform), Without<StaticCollider>>,
+    mut commands: Commands,
+    kinematics: Query<(&KinematicCollider, &mut Transform), (Without<StaticCollider>, Without<Projectile>)>,
+    bullets: Query<(&KinematicCollider, &mut Transform, Entity), (Without<StaticCollider>, With<Projectile>)>,
     statics: Query<(&StaticCollider, &Transform), (Without<KinematicCollider>, Without<Player>)>,
 ) {
     for (kc, mut kt) in kinematics {
@@ -57,6 +60,23 @@ pub fn do_collisions(
             if colliding {
                 kt.translation = kt.translation
                     + find_mtv(&transformed_kc_shape, &transformed_sc_shape).extend(0.);
+            }
+        }
+    }
+
+    for (kc, mut kt, this) in bullets {
+        for (sc, st) in &statics {
+            let mut transformed_kc_shape = kc.shape.clone();
+            transformed_kc_shape.min += kt.translation.truncate();
+            transformed_kc_shape.max += kt.translation.truncate();
+
+            let mut transformed_sc_shape = sc.shape.clone();
+            transformed_sc_shape.min += st.translation.truncate();
+            transformed_sc_shape.max += st.translation.truncate();
+
+            let colliding = transformed_kc_shape.intersects(&transformed_sc_shape);
+            if colliding {
+                commands.entity(this).despawn();
             }
         }
     }
